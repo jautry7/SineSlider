320ad8f2ef52d483f55e9a51572fbf57f0922427
John Autry <27386573+jautry7@users.noreply.github.com>
2026-09-16 22:17:16 -0400

    Created skeleton project for native version

---
+++ native/SineSlider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SineSlider
+//
+//  Created by John Autry on 9/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ native/SineSlider/SineSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SineSliderApp.swift
+//  SineSlider
+//
+//  Created by John Autry on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SineSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
